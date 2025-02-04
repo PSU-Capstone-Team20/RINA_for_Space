285a920b76b6ba78b17bbbf331bc0aa39ddfbdeb
Dominic Pasquarelli <info@dominicpasquarelli.com>
2025-02-04 18:29:33 -0500

    Added dif_manager

---
+++ src/dif_manager.ads
@@ -0,0 +1,28 @@
+with application; use application;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Containers.Vectors;
+with Ada.Text_IO; use Ada.Text_IO;
+
+package dif_manager is
+    type DIF_MANAGER is tagged;
+
+    package DIF_Vectors is new Ada.Containers.Vectors
+      (Index_Type => Natural, Element_Type => DIF_Access);
+    subtype DIF_Vector is DIF_Vectors.Vector;
+
+    type DIF_MANAGER is tagged record
+        DIFs : DIF_Vector;
+    end record;
+
+    -- creates a DIF with specified ID and adds it to the manager
+    procedure Create_DIF(ID : Integer; Manager : in out DIF_MANAGER);
+    -- creates a DIF with argument for name and adds it to the manager
+    procedure Create_Named_DIF(ID : Integer; Name : Unbounded_String; Manager : in out DIF_MANAGER);
+    -- returns the ID of the DIF in the manager
+    function Get_ID(DIF : DIF_Access) return Integer;
+    -- returns the name of the DIF in the manager
+    function Get_Name(DIF : DIF_Access) return Unbounded_String;
+    -- deletes the DIF that has the specified ID from the manager
+    procedure Disconnect_DIF(ID : Integer; Manager : in out DIF_MANAGER);
+
+end dif_manager;
