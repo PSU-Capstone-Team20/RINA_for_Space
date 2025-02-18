2a604c45512400fbf90eda04a59aee614934337d
Glennkabob <glennbart02@gmail.com>
2025-02-18 15:45:44 -0500

    reconfigured RIB ads and adb to instead use string representations of difs, ipcps, and apns as opposed to copies, wrote add, get, delete, and update procedures/functions, created display_map stub, wrote Get_Map function, fixed naming error and other miscellaneous bugs

--- src/rib.adb
+++ src/rib.adb
@@ -1,3 +1,4 @@
+with Ada.Containers;
 with Ada.Containers.Vectors;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Containers.Indefinite_Hashed_Maps;
@@ -13,47 +14,95 @@ package body RIB is
 
    -- TODO: logic into each procedure and functions 
 
-      map : Map;
+      map : RIB_Hashed_Maps.Map;
    
     --add procedures for RIB_Entry/DIF/IPCP/APN
     procedure Add_Entry(Name : Unbounded_String) is
       item : RIB_Entry;
     begin
-      map.Include(Name'Image, item);
+      map.Include(Name, item);
     end Add_Entry;
 
-    procedure Add_DIF(dif : dif.DIF; item : RIB_Entry) is
+    procedure Add_DIF(dif : Unbounded_String; item : out RIB_Entry) is
     begin
       item.Obj_Type.Connected_DIFs.Append(dif);
     end Add_DIF;
     
-    procedure Add_IPCP(ipcp : IPCP.ipcp; item : RIB_Entry);
-    procedure Add_APN(APN : application.application; item : RIB_Entry);
+    procedure Add_IPCP(ipcp : Unbounded_String; item : out RIB_Entry) is
+    begin
+      item.Obj_Type.Accessible_IPCPs.Append(ipcp);
+    end Add_IPCP;
+    procedure Add_APN(APN : Unbounded_String; item : out RIB_Entry) is
+    begin
+      item.Obj_Type.Active_APNs.Append(APN);
+    end Add_APN;
 
     --get functions for RIB_Entry/DIF/IPCP/APN
-    function Get_Entry(Name: Unbounded_String) return RIB_Entry;
-    function Get_DIF(index : Integer; item : RIB_Entry) return dif.DIF;
-    function Get_IPCP(index : Integer; item : RIB_Entry) return IPCP.ipcp;
-    function Get_APN(index : Integer; item : RIB_Entry) return application.application;
+    function Get_Entry(Name: Unbounded_String) return RIB_Entry is
+    begin
+      return map(Name);
+    end Get_Entry;
+    function Get_DIF(index : Integer; item : RIB_Entry) return Unbounded_String is
+    begin
+      return item.Obj_Type.Connected_DIFs(index);
+    end Get_DIF;
+    function Get_IPCP(index : Integer; item : RIB_Entry) return Unbounded_String is
+    begin
+      return item.Obj_Type.Accessible_IPCPs(index);
+    end Get_IPCP;
+    function Get_APN(index : Integer; item : RIB_Entry) return Unbounded_String is
+    begin
+      return item.Obj_Type.Active_APNs(index);
+    end Get_APN;
 
     --delete procedures for RIB_Entry/DIF/IPCP/APN
-    procedure Delete_Entry(Name: Unbounded_String);
-    procedure Delete_DIF(index : Integer; item : RIB_Entry);
-    procedure Delete_IPCP(index : Integer; item : RIB_Entry);
-    procedure Delete_APN(index : Integer; item : RIB_Entry);
+    procedure Delete_Entry(Name: Unbounded_String) is
+    begin
+      RIB_Hashed_Maps.Delete(map, Name);
+    end Delete_Entry;
+    procedure Delete_DIF(index : Integer; item : out RIB_Entry) is
+    begin
+      item.Obj_Type.Connected_DIFs.Delete(index);
+    end Delete_DIF;
+    procedure Delete_IPCP(index : Integer; item : out RIB_Entry) is
+    begin
+      item.Obj_Type.Accessible_IPCPs.Delete(index);
+    end Delete_IPCP;
+    procedure Delete_APN(index : Integer; item : out RIB_Entry) is
+    begin
+      item.Obj_Type.Active_APNs.Delete(index);
+    end Delete_APN;
 
     --update procedures for RIB_Entry/DIF/IPCP/APN
-    procedure Update_Entry(Name: Unbounded_String; item : RIB_Entry);
-    procedure Update_DIF(index : Integer; item : RIB_Entry; dif : dif.DIF);
-    procedure Update_IPCP(index : Integer; item : RIB_Entry; ipcp : IPCP.ipcp);
-    procedure Update_APN(index : Integer; item : RIB_Entry; APN : application.application);
+    procedure Update_Entry(Name: Unbounded_String; item : RIB_Entry) is
+    begin
+      map(Name) := item;
+    end Update_Entry;
+    procedure Update_DIF(index : Integer; item : out RIB_Entry; dif : Unbounded_String) is
+    begin
+      item.Obj_Type.Connected_DIFs(index) := dif;
+    end Update_DIF;
+    procedure Update_IPCP(index : Integer; item : out RIB_Entry; ipcp : Unbounded_String) is
+    begin
+      item.Obj_Type.Accessible_IPCPs(index) := ipcp;
+    end Update_IPCP;
+    procedure Update_APN(index : Integer; item : out RIB_Entry; APN : Unbounded_String) is
+    begin
+      item.Obj_Type.Active_APNs(index) := APN;
+    end Update_APN;
     
     --prints the entire hashed map
-    procedure Display_Map;
+    procedure Display_Map is
+    begin
+      null;
+    end Display_Map;
     --TODO: adding procedure for displaying specific map that is being searched.
 
     --gets the entire map
-    function Get_map return Map;
+    function Get_map return RIB_Hashed_Maps.Map is
+    begin
+      return map;
+    end Get_map;
  
 
 
--- src/rib.ads
+++ src/rib.ads
@@ -1,7 +1,8 @@
+with Ada.Containers.Ordered_Multisets;
 with Ada.Containers.Vectors;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Containers.Indefinite_Hashed_Maps;
-with Ada.Strings.Hash;
+with Ada.Strings.Unbounded.Hash;
 with dif;
 with ipcp;
 with application;
@@ -12,17 +13,17 @@ package RIB is
     
     -- vectors that contain the applications/difs/ipcps that exist on each rib object
     package Application_Vectors is new Ada.Containers.Vectors
-     (Index_Type => Natural, Element_Type => application.application);
+     (Index_Type => Natural, Element_Type => Ada.Strings.Unbounded.Unbounded_String);
     package DIF_Vectors is new Ada.Containers.Vectors
-     (Index_Type => Natural, Element_Type => dif.DIF);
+     (Index_Type => Natural, Element_Type => Ada.Strings.Unbounded.Unbounded_String);
     package IPCP_Vectors is new Ada.Containers.Vectors
-     (Index_Type => Natural, Element_Type => ipcp.ipcp);
+     (Index_Type => Natural, Element_Type => Ada.Strings.Unbounded.Unbounded_String);
 
     -- represents a live computer in the system as a RIB object
     type RIB_Obj is record 
-      Accessible_IPCPs : IPCP_Vectors; -- list of ipcps system has access to
-      Connected_DIFs  : DIF_Vectors; -- list of difs system has access to
-      Active_APNs  : Application_Vectors; -- applications running on system
+      Accessible_IPCPs : IPCP_Vectors.Vector; -- list of ipcps system has access to
+      Connected_DIFs  : DIF_Vectors.Vector; -- list of difs system has access to
+      Active_APNs  : Application_Vectors.Vector; -- applications running on system
     end record;
 
     type RIB_Entry is record
@@ -34,7 +35,7 @@ package RIB is
     (
       Key_Type => Unbounded_String,
       Element_Type => RIB_Entry,
-      Hash => Ada.Strings.Hash,
+      Hash => Ada.Strings.Unbounded.Hash,
       Equivalent_Keys => "="
     );
     use RIB_Hashed_Maps;
@@ -51,33 +52,33 @@ package RIB is
 
     --add procedures for RIB_Entry/DIF/IPCP/APN
     procedure Add_Entry(Name : Unbounded_String);
-    procedure Add_DIF(dif : dif.DIF; item : RIB_Entry);
-    procedure Add_IPCP(ipcp : ipcp.ipcp; item : RIB_Entry);
-    procedure Add_APN(APN : application.application; item : RIB_Entry);
+    procedure Add_DIF(dif : Unbounded_String; item : out RIB_Entry);
+    procedure Add_IPCP(ipcp : Unbounded_String; item : out RIB_Entry);
+    procedure Add_APN(APN : Unbounded_String; item : out RIB_Entry);
 
     --get functions for RIB_Entry/DIF/IPCP/APN
     function Get_Entry(Name: Unbounded_String) return RIB_Entry;
-    function Get_DIF(index : Integer; item : RIB_Entry) return dif.DIF;
-    function Get_IPCP(index : Integer; item : RIB_Entry) return ipcp.ipcp;
-    function Get_APN(index : Integer; item : RIB_Entry) return application.application;
+    function Get_DIF(index : Integer; item : RIB_Entry) return Unbounded_String;
+    function Get_IPCP(index : Integer; item : RIB_Entry) return Unbounded_String;
+    function Get_APN(index : Integer; item : RIB_Entry) return Unbounded_String;
 
     --delete procedures for RIB_Entry/DIF/IPCP/APN
     procedure Delete_Entry(Name: Unbounded_String);
-    procedure Delete_DIF(index : Integer; item : RIB_Entry);
-    procedure Delete_IPCP(index : Integer; item : RIB_Entry);
-    procedure Delete_APN(index : Integer; item : RIB_Entry);
+    procedure Delete_DIF(index : Integer; item : out RIB_Entry);
+    procedure Delete_IPCP(index : Integer; item : out RIB_Entry);
+    procedure Delete_APN(index : Integer; item : out RIB_Entry);
 
     --update procedures for RIB_Entry/DIF/IPCP/APN
     procedure Update_Entry(Name: Unbounded_String; item : RIB_Entry);
-    procedure Update_DIF(index : Integer; item : RIB_Entry; dif : dif.DIF);
-    procedure Update_IPCP(index : Integer; item : RIB_Entry; ipcp : ipcp.ipcp);
-    procedure Update_APN(index : Integer; item : RIB_Entry; APN : application.application);
+    procedure Update_DIF(index : Integer; item : out RIB_Entry; dif : Unbounded_String);
+    procedure Update_IPCP(index : Integer; item : out RIB_Entry; ipcp : Unbounded_String);
+    procedure Update_APN(index : Integer; item : out RIB_Entry; APN : Unbounded_String);
     
     --prints the entire hashed map
     procedure Display_Map;
     --TODO: Display procedure for specific entries that are being searched.
 
     --gets the entire map
-    function Get_map return Map;
+    function Get_map return RIB_Hashed_Maps.Map;
  
 end RIB;
