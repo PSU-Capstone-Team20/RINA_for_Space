d85791f167401833361375fbb38dac1068eccd6f
Hyelin L <143141463+mashimelo@users.noreply.github.com>
2025-04-14 16:46:28 -0700

    added comments

--- src/ipc_data_transfer.adb
+++ src/ipc_data_transfer.adb
@@ -8,30 +8,43 @@ with IPCP_Types; use IPCP_Types;
 
 package body IPC_Data_Transfer is
 
---SDU delmiting
+--SDU delmiting extracts SDU from raw byte array 
 procedure Delimit_SDU(Raw_Data : in Byte_Array; SDU : out Byte_Array) is
+   --first byte of input data to indicate the length of SDU
    SDU_Length : Natural := Natural (Raw_Data (1));
 begin
+   --take SDU portion based on length, skip length byte 
    SDU := Raw_Data(2 .. SDU_Length + 1);
 end Delimit_SDU;
 
---EFCP both DTP and DTCP 
+--EFCP both DTP and DTCP for fragmenting SDU into PDUs
 procedure DTP(SDU : in Byte_Array; Fragment_PDU : out PDU_List) is
+   -- calculates number of fragments to divide the SDU based on maximum PDU size 
    Frag_Num : Natural := (SDU'Length + Max_PDU_Size -1) / Max_PDU_Size;
+   --used for tracking current fragment indeex
    Frag_Index : Natural := 1;
+   --setting starting byte index
    First_Byte : Positive := SDU'First;
+   --declare variable to hold ending byte index for each fragment 
    Last_Byte : Positive;
    
 begin
+   -- initialize PDU list with either empty or default values for each fragment 
    Fragment_PDU := (1 .. Frag_Num => (others => <>));
+   --loop for each fragment to make PDU list
    for I in 1 .. Frag_Num loop
+      --calculate last byte for current fragment
       Last_Byte := First_Byte + Max_PDU_Size -1;
+      --if last byte index is above SDU's actual size, adjust the last byte 
       if Last_Byte > SDU'Last then 
          Last_Byte := SDU'Last;
       end if;
       declare
+         -- fragment byte array that holds the sliced chunk of SDU
          Frag_Data : Byte_Array (1 .. Last_Byte - First_Byte + 1);
-         Convert_Data : String(1 .. Last_Byte - First_Byte + 1);          
+         -- makes string version of fragment to be used 
+         Convert_Data : String(1 .. Last_Byte - First_Byte + 1);
+         --default PCI filled with placeholder values          
          Default_PCI : Transport_Types.PCI_T := (Src_CEP_ID => To_Unbounded_String(""), 
                                                     Dst_CEP_ID => To_Unbounded_String(""), 
                                                     Seq_Num => 0, 
@@ -42,33 +55,46 @@ begin
                                                     Ack_Req => False, 
                                                     Retransmit => False, 
                                                     Timestamp => 0);
+         --preparing string buffer for fragment ID 
          Fragment_ID : String(1 ..7);
+         --converting current loop index to string to use as an ID 
          Temp_ID : constant String := Integer'Image(I);
       begin
+         --take actual byte from SDU for fragment
          Frag_Data := SDU (First_Byte .. Last_Byte);
+         --for loop to convert each byte into a character 
          for J in Convert_Data'Range loop
+            --converting each byte in Frag_Data to character value 
             Convert_Data(J) := Character'Val(Frag_Data(J));
          end loop;
-         Fragment_ID := "Fragment" & Temp_ID(Temp_ID'First + 1 .. Temp_ID'Last);  
+         --for creating fragment strings and using slicing of image to remove leading space 
+         Fragment_ID := "Fragment" & Temp_ID(Temp_ID'First + 1 .. Temp_ID'Last);
+         --storing the made fragment into the PDU list at index I using ID, default PCI and the converted data 
          Fragment_PDU(I) := (ID => Fragment_ID,
                              PCI => Default_PCI,
                              Data => To_Unbounded_String(Convert_Data));
-         
+         --update the first byte index for the next coming fragment
          First_Byte := Last_Byte + 1;
       end;
    end loop;
 end DTP;
 
+--procedure for transmission control on a PDU
 procedure DTCP(PDU : in out EFCP.PDU_S_T) is
+   --the placeholder for current time 
    Real_Time : Natural := 0;
+   --gets timestamp from PDU's PCI to get last active 
    Last_Active_Time : Natural := PDU.PCI.Timestamp;
    Max_Time : constant Natural := 1000; -- timeout threshold
 
 begin
+   --if active time is greater than max time this would mark PDU for retransmission and request the acknowledgement 
+   --interpretation of Delta-t
    if Real_Time - Last_Active_Time > Max_Time then
       PDU.PCI.Ack_Req := True;
       PDU.PCI.Retransmit := True;
    else
+      --if it is within the time this would request just the acknowledgement 
       PDU.PCI.Ack_Req := True;
    end if;
    
@@ -77,6 +103,7 @@ end DTCP;
 --relay pdu
 procedure Relay_PDU(PDU : in out EFCP.PDU_S_T) is 
 begin
+   --logging destination CEP ID the PDU that is being relayed to it 
    Put_Line(To_String("[Relay_PDU] : Relay PDU to  " & PDU.PCI.Dst_CEP_ID));
 end Relay_PDU;
 
@@ -84,6 +111,7 @@ end Relay_PDU;
 procedure Multiplex_PDU(PDU : in EFCP.PDU_S_T; Lower_Flow_ID : out Flow_ID) is
 
 begin
+   --convert source CEP ID to integer and applies modulo 10 to get the flow then assigns it to Lower_Flow_ID
    Lower_Flow_ID := Flow_ID(Integer'Value(To_String(PDU.PCI.Src_CEP_ID)) mod 10);
 end Multiplex_PDU;
 
--- src/rina_bp_bundle.adb
+++ src/rina_bp_bundle.adb
@@ -9,7 +9,7 @@ with RIB;
 
 package body Rina_BP_Bundle is
 
-   --create bundle
+   --create bundle function with all the information: header, source/destination, payload, and path
    function Create_Bundle(Version : Natural; 
                           Processing_Flag : Integer; 
                           Block_Length : Integer; 
@@ -17,6 +17,7 @@ package body Rina_BP_Bundle is
                           Dst_EID : String; 
                           Payload : String;
                           Path    : Path_Vectors.Vector) return Bundle is
+      --variable B of type Bundle to construct and return values
       B : Bundle;
    begin
       B.Header.Version := Version;
@@ -24,6 +25,7 @@ package body Rina_BP_Bundle is
       B.Header.Block_Length := Block_Length;
       B.Src_EID(1 .. Src_EID'Length) := Src_EID;
       B.Dst_EID(1 .. Dst_EID'Length) := Dst_EID;
+      --converts each character in payload string to ASCII value and stores into payload array
       for I in Payload'Range loop
          B.Payload(I) := Character'Pos(Payload(I));
       end loop;
@@ -33,19 +35,29 @@ package body Rina_BP_Bundle is
 
    --procedure for sending bundle without the streams util
    procedure Send_Bundle(B : in Bundle) is
+      --byte array of bundle payload
       SDU : Byte_Array(B.Payload'Range);
+      --array of 10 PDUs for holding fragmented data 
       PDUs : PDU_List(1 .. 10);
+      --holding assigned flow id from multiplexing function
       Flow : Flow_ID;
    begin
+      --converting payload into SDU 
       Delimit_SDU(B.Payload, SDU);
+      --using data transfer protocol DTP to fragment SDU into list of PDUs
       DTP(SDU => SDU, Fragment_PDU => PDUs);
 
+      --for loop that iterates through each PDU to apply the transmission control, multiplex, and relay 
       for I in PDUs'Range loop
+         -- assigns first hop's name from path as destination 
          if B.Path.Length > 0 then 
             PDUs(I).PCI.Dst_CEP_ID := B.Path.Element(0).Element(0).Name;
          end if;
+         --retransmissions and ack policy applied
          DTCP(PDUs(I));
+         --mapping PDU to specific flow id depending on its source id 
          Multiplex_PDU(PDUs(I), Flow);
+         --forwarding PDU to next node 
          Relay_PDU(PDUs(I));
       end loop;
       
@@ -54,32 +66,43 @@ package body Rina_BP_Bundle is
       Put_Line("[Send_Bundle] Bundle sent successfully");
    end Send_Bundle;
 
-   --function for receiving bundle 
+   --function for receiving bundle: receiving array of PDUs and reassemble into single byte array 
    procedure Receive_Bundle(PDUs : in out PDU_List; Reassemble : out Byte_Array) is
+      --holding total length of data frags to get size of final output buffer 
       Total_Length : Natural := 0;
       
    begin
+      --loop for each received PDU
       for I in PDUs'Range loop
+         --DTCP procedure is applied here 
          DTCP(PDUs(I));
          declare
+            --converts data payload of PDU from unbounded string to string 
             Temp_String : constant String := To_String(PDUs(I).Data);
          begin
+            --takes total length of all PDU data 
             Total_Length := Total_Length + Temp_String'Length;
          end;
       end loop;
+      --final Byte_Array to hold the reassembled payload is initialized here 
       Reassemble := (1 .. Total_Length => 0);
 
       declare
+         --for tracking the insertion points into reassumbled byte array 
          Offset : Positive := 1;
          
       begin
-          
+          --for iterating through each PDU to copy data into final byte array 
          for I in PDUs'Range loop
             declare
+               --converting unbounded string data to string
                Temp_String : constant String := To_String(PDUS(I).Data);
             begin
+               --iterating through the characters in string 
                for J in Temp_String'Range loop
+                  --converting each character to ASCII value and place into final byte array 
                   Reassemble(Offset) := Character'Pos(Temp_String(J));
+                  --insertion point moved forward 
                   Offset := Offset + 1;
                end loop;
             end;
